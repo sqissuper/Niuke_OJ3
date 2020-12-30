f3be4540122075e431a5cb12e49ee62d7a6de330
sqissuper <1764491917@qq.com>
2020-12-30 21:48:52 +0800

    Practice_Niuke

---
+++ TextDemo6.java
@@ -0,0 +1,73 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class TextDemo6 {
+
+    //奇数变1，偶数变0
+    public static void main(String[] args){
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int num = 10,ret;
+//        int res = 0; //接收结果
+//        int[] arr = new int[10];//存放临时结果
+//        int i = 0;
+//
+//        while(n != 0){
+//            ret = n % num;
+//            n /= num;
+//            if(ret % 2 == 0){
+//                arr[i] = 0;
+//            }else{
+//                arr[i] = 1;
+//            }
+//            i++;
+//        }
+//        for(int j = 0; j < arr.length; j++){
+//            res += (int)arr[j]*Math.pow(num,j);
+//        }
+//        System.out.println(res);
+
+
+        //最大公约数与最小公倍数之和
+//        long n = sc.nextInt();
+//        long m = sc.nextInt();
+//        long m1 = m;
+//        long n1 = n;
+//        while(m % n != 0){
+//            long tmp = m % n;
+//            m = n;
+//            n = tmp;
+//        }
+//        System.out.println(n + (m1*n1 / n));
+
+
+        //小乐乐上电梯
+//        int n = sc.nextInt();
+//        int time = 4;//一趟电梯的事件
+//        if(n < 12){
+//            time /= 2;
+//        }else{
+//            n /= 12;
+//            time = time * n + time / 2;
+//        }
+//        System.out.println(time);
+
+    //输出一行，为正整数n表示为六进制的结果
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        long n = sc.nextInt();//13
+//        long t = 1,ret = 0,sum = 0;
+//        while(n > 0){
+//            ret = n % 6;
+//            ret *= t;
+//            sum += ret;
+//            t *= 10;
+//            n /= 6;
+//        }
+//        System.out.println(sum);
+//
+    }
+
+}
